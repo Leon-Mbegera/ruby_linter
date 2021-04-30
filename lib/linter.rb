ca26204277ce08a70f1069dc2d5eabf20b69ca30
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-04-30 12:09:27 +0300

    Correct num arguments

--- lib/linter.rb
+++ lib/linter.rb
@@ -10,18 +10,18 @@ class Stylint
   end
 
   def check
-    column = 0
+    num = 0
     File.readlines(@file_path).each do |line|
-      column += 1
-      comment_check(line, column)
-      ending_check(line, column)
-      colon_space_check(line, column)
-      empty_space_check(line, column)
-      unit_check(line, column)
-      space_check(line, column)
-      start_space_check(line, column)
+      num += 1
+      comment_check(line, num)
+      ending_check(line, num)
+      colon_space_check(line, num)
+      empty_space_check(line, num)
+      unit_check(line, num)
+      space_check(line, num)
+      start_space_check(line, num)
       start_bracket_check(line, column)
-      end_bracket_check(line, column)
+      end_bracket_check(line, num)
     end
     if @errors.length.zero?
       @errors << "All checks have passed. ".green
