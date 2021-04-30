1819800547adb7e39a31e66f9cc10f8a1c4be5fd
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-04-30 12:12:03 +0300

    Define comment check method

--- lib/style_linter.rb
+++ lib/style_linter.rb
@@ -1,5 +1,12 @@
 require 'colorize'
 
 module StyleLinter
+
+  def comment_check(line, num)
+    return unless line.include?('/*')
+
+    @errors << "#{'WARNING'.yellow} :Remove comments on line #{num}.\n"
+  end
+
   
 end
