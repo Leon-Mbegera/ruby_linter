8c36c56053bcb327ddf4fef30a97defdc3173508
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-04-30 12:21:58 +0300

    Define space checking method

--- lib/style_linter.rb
+++ lib/style_linter.rb
@@ -8,5 +8,11 @@ module StyleLinter
     @errors << "#{'WARNING'.yellow} :Remove comments on line #{num}.\n"
   end
 
+  def space_check(line, num)
+    return unless line ~= /\A\w/ && !line.start_with?(' ')
+
+    @errors << "#{'ERROR'.red} :Two spaces needed at the start of line #{num}\n"
+  end
+
   
 end
