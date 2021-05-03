b0ad70bfbdbe8ed2f803c059035e0af929c4b396
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-03 15:19:15 +0300

    Define bracket check methods for linter

---
+++ spec/style_linter_spec.rb
@@ -0,0 +1,69 @@
+require_relative '../lib/linter'
+require_relative '../lib/style_linter'
+
+describe Stylint do
+
+  let(:checker) { Stylint.new('../Assets/bad_styles.css')}
+
+  describe '#comment check method' do
+    context 'when there is comment' do
+      it 'checks if there is comments on the line' do
+        contain = checker.send(:comment_check, '/*', 5)
+        expect(contain).not_to eq(['Remove comments on line 5'])
+      end
+    end
+  end
+
+  describe '#space check method' do
+    context 'when there is two spaces before main content line' do
+      it 'checks for two spaces at beginning of line' do
+        contain = checker.send(:space_check, ' ', 13)
+        expect(contain).not_to eq(['Two spaces needed at the start of line 13'])
+      end
+    end
+  end
+
+  describe '#unit check method' do
+    context 'when rem or em used instead of pixels' do
+      it 'checks for other measurement unit besides px' do
+        contain = checker.send(:unit_check, 'rem', 21)
+        expect(contain).not_to eq(['Recommended use of rem or em for responsiveness on line 21'])
+      end
+    end
+  end
+
+  describe '#start space check method' do
+    context 'when no space before line' do
+      it 'checks for a closing curly brace at end of line' do
+        contain = checker.send(:start_space_check, '}', 12)
+        expect(contain).not_to eq(['Remove empty space at start of line 12'])
+      end
+
+      it 'checks for a period symbol at start of line' do
+        contain = checker.send(:start_space_check, '.', 3)
+        expect(contain).not_to eq(['Remove empty space at start of line 3'])
+      end
+    end
+  end
+
+  describe '#colon space check method' do
+    context 'when no space after colon' do
+      it 'checks for spaces after :' do
+        contain = checker.send(:colon_space_check, ' ', 27)
+        expect(contain).not_to eq(['Extra space needed after colon on line 27'])
+      end
+    end
+  end
+
+  describe '#start bracket check method' do
+    context 'when there is two spaces' do
+      it 'checks for two spaces before { ' do
+        contain = checker.send(:start_bracket_check, '{', 26)
+      end
+    end
+  end
+
+  
+
+
+end
