fe6cd3968c6a8b6de0816b928910330f442f92f6
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-05-04 12:52:20 +0300

    Make corrections for linter check methods

--- lib/style_linter.rb
+++ lib/style_linter.rb
@@ -8,7 +8,7 @@ module StyleLinter
   end
 
   def space_check(line, num)
-    return unless line =~ /\A\w/ && !line.start_with?(' ')
+    return unless line =~ /\A\w/ && !line.start_with?('  ')
 
     @errors << "#{'ERROR'.red} :Two spaces needed at the start of line #{num}\n"
   end
@@ -20,37 +20,33 @@ module StyleLinter
   end
 
   def start_space_check(line, num)
-    return unless line.start_with?(' ') && (line.end_with?('.') || line.end_with?('}'))
-
-    @errors << "#{'ERROR'.red} :Remove empty space at start of line #{num}\n"
+    @errors << "#{'ERROR'.red}  :Remove empty space at start of line #{num}\n " if line.start_with?(' ') && (line.include?('.') || line.include?('}'))
   end
 
   def colon_space_check(line, num)
     return unless line.include?(':')
 
     point = line.split(':')[1]
-    @errors << "#{'ERROR'.red} :Extra space needed after colon on line #{num}" if point[0] != ' '
+    @errors << "#{'ERROR'.red} :Extra space needed after colon on line #{num}\n" if point[0] != ' '
   end
 
   def start_bracket_check(line, num)
-    stripped = line.delete('\n')
+    stripped = line.delete("\n")
     return unless line.include?('{') && !stripped.end_with?('{')
 
     @errors << "#{'ERROR'.red} :Expected '{' at the end of line #{num}. Any more code should fall on next line.\n"
   end
 
   def end_bracket_check(line, num)
-    stripped = line.delete('\n')
+    stripped = line.delete("\n")
     return unless line.include?('}') && !stripped.end_with?('}')
 
     @errors << "#{'ERROR'.red} :Expected '}' at the end of line #{num}\n"
   end
 
   def empty_space_check(line, num)
-    stripped = line.delete('\n')
-    return unless stripped.end_with?(' ')
-
-    @errors << "#{'ERROR'.red} :Remove trailing spaces at end of line #{num}\n"
+    stripped = line.delete("\n")
+    @errors << "#{'ERROR'.red}  :Remove trailing spaces at end of line #{num}\n" if stripped.end_with?(' ')
   end
 
   def ending_check(line, num)
